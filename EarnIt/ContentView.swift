12d0b00ac4b7f7301ff418b7808ca3088767e2e0
jonesrmj <51058123+jonesrmj@users.noreply.github.com>
2020-10-05 10:59:20 -0400

    Initial Commit

---
+++ EarnIt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EarnIt
+//
+//  Created by Ryan Jones on 10/5/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EarnIt/EarnItApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarnItApp.swift
+//  EarnIt
+//
+//  Created by Ryan Jones on 10/5/20.
+//
+
+import SwiftUI
+
+@main
+struct EarnItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
